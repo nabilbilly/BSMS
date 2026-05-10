64a739690220cd31e2b5a30d5fdf1eb366c82637
nabilbilly <100012984+nabilbilly@users.noreply.github.com>
2026-05-10 16:06:38 +0000

    solve

--- alembic/env.py
+++ alembic/env.py
@@ -8,7 +8,17 @@ from alembic import context
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+database_url = os.getenv("DATABASE_URL")
 
+if database_url:
+    if database_url.startswith("postgres://"):
+        database_url = database_url.replace(
+            "postgres://",
+            "postgresql://",
+            1
+        )
+
+    config.set_main_option("sqlalchemy.url", database_url)
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
